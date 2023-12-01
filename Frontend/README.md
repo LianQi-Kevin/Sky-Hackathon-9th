eec98794f0ccdc9f91549f719e14730be1d0012d
LianQi-Kevin <18201584863@163.com>
2023-12-01 18:29:46 +0800

    * init VUE3-router project * add README.md

---
+++ Frontend/README.md
@@ -0,0 +1,13 @@
+# Sky-Hackathon-9th Frontend project
+
+
+
+---
+
+### Resources
+
+* [NVIDIA NGC SDXL](https://catalog.ngc.nvidia.com/orgs/nvidia/teams/ai-foundation/models/sdxl/api)
+* [Element Plus](https://element-plus.org/zh-CN/)
+* [iconfont](https://www.iconfont.cn/)
+* [screenshot-to-code](https://github.com/abi/screenshot-to-code)
+* [chat to frontend page](https://v0.dev/)
---
+++ Frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <div class="common-layout">
+        <el-container>
+            <el-header>Header</el-header>
+            <el-container>
+                <el-aside width="200px">Aside</el-aside>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+<style>
+
+</style>
